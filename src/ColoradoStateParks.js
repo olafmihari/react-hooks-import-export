4e5e7dfb2f987f2a62a9d18edc30145174c79424
Olaf Mihari <olafnjenga@gmail.com>
2023-04-18 11:02:29 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      <p>{trees}</p>
+      <p>{wildlife}</p>
+      {RMFunctions.elevation()}
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
